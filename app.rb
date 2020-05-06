bca1872413a87a2e65045e5863572a8550e4c5a1
Eric Johnson <ericjohnson0216@icloud.com>
2020-05-06 01:50:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,15 +3,6 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/name' do
-    @name = Name.all
-
-    if req.path.match(/name/)
-      @name.each do |name|
-        resp.write "My name is #{item}\n"
-      end
-
-    # name each do
-    #   get '/name'
-    # erb :'medicines/index.html.erb'
+    
   end
 end
