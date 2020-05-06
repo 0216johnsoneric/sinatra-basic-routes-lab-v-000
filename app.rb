754d31c24afe6ee6fa015e13428be84fdf9e472a
Eric Johnson <ericjohnson0216@icloud.com>
2020-05-06 01:29:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/name' do
-    @medicines = name.all
+    @medicines = Name.all
 
     erb :'medicines/index.html.erb'
   end
